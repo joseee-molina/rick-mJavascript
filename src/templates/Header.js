ecedfb3bb16d4c4744e2282885af30c40f88c346
Octavio Molina <jose.octavio@ciencias.unam.mx>
2021-06-19 17:06:08 -0500

    sending everything before travisCS

---
+++ src/templates/Header.js
@@ -0,0 +1,21 @@
+const Header = () => {
+    const view = ` 
+        <div class="Header-main">
+            <div class="HEader-logo">
+                <h1>
+                    <a href="/">
+                        100tifi.co
+                    </a>
+                </h1>
+            </div>
+            <div class="Header-nav">
+                <a href="#/abour/">
+                    About
+                <a/>
+            </div>
+        </div>
+    `;
+    return view;
+};
+
+export default Header;
